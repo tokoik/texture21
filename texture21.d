b907f9ebf44fa73b8ba45ef69f880b8fbbc6a169
Kohe Tokoi <tokoik@gmail.com>
2012-06-26 23:50:08 +0900

    update dependency

---
+++ texture21.d
@@ -0,0 +1,8 @@
+main.o: main.cpp normalmap.h normalizemap.h trackball.h sphere.h
+main1.o: main1.cpp normalmap.h normalizemap.h trackball.h sphere.h
+matrix.o: matrix.cpp matrix.h
+normalizemap.o: normalizemap.cpp normalizemap.h
+normalmap.o: normalmap.cpp normalmap.h
+sphere.o: sphere.cpp sphere.h matrix.h
+sphere1.o: sphere1.cpp sphere.h matrix.h
+trackball.o: trackball.cpp trackball.h
